648d5e43bfe52d2bba0472dd44becb23f81ca582
Khavin Shankar <khavinshankar@gmail.com>
2024-10-19 00:53:30 +0530

    updated migrations as per the changes in pre-plug

--- abdm/migrations/0014_abhanumber_mobile.py
+++ abdm/migrations/0014_abhanumber_mobile.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("abdm", "0014_replace_0013"),
+        ("abdm", "0013_abhanumber_patient"),
     ]
 
     operations = [
--- abdm/migrations/0015_remove_abhanumber_txn_id_transaction.py
+++ abdm/migrations/0015_remove_abhanumber_txn_id_transaction.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ("abdm", "0015_abhanumber_mobile"),
+        ("abdm", "0014_abhanumber_mobile"),
     ]
 
     operations = [
